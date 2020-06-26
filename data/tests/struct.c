ece3416a25b21b4198155687cd17d12cba0120b0
Paul Duncan <pabs@pablotron.org>
2020-06-26 19:56:35 -0400

    pwasm-dynasm-jit.dasc: fix calls in unreachable and global.{get,set}

--- data/tests/struct.c
+++ data/tests/struct.c
@@ -2,19 +2,18 @@
 #include <stdint.h>
 
 typedef struct {
-  uint64_t a, b, c;
-  uint32_t d, e, f;
+  uint64_t a, b;
 } test_t;
 
 uint64_t do_stuff(uint32_t a, uint32_t b, const test_t t) {
-  return a + b + t.a + t.c;
+  return a + b + t.a + t.b;
 }
 
 int main(int argc, char *argv[]) {
   (void) argc;
   (void) argv;
 
-  test_t t = { .a = 12, .c = 34, .e = 56 };
+  test_t t = { .a = 12, .b = 34 };
   do_stuff(22, 15, t);
 
   return 0;
